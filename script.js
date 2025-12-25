d9e88a1b47e63fa3133fef665d058976d44e277a
Ritik Sharma <ritiksharma10508@gmail.com>
2025-12-26 01:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const byteSize = (str) => {
   // write your code here
-	return new blob([str]).size;
+	return byteSize([str]).size;
 };
 
-// Do not change the code below
+Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
